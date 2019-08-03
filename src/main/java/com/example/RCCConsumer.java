fea7fcf31bd275e4a094e0ec3d8d84db951613ac
Sergey Kirpichenok <skirpichenok@gmail.com>
2019-08-03 22:41:33 +0300

    2

--- src/main/java/com/example/RCCConsumer.java
+++ src/main/java/com/example/RCCConsumer.java
@@ -20,12 +20,16 @@ import java.util.Date;
 public class RCCConsumer {
 
     private static final String RCC_INFO_SQL = "select json_agg(t) from (" +
-            "select item_data.id as item_data_id, item_data.tenant_id as tenant_id, study.name as study_name, study.id as study_id, " +
-            "study_site.id as study_site_id, site.name as study_site_name, " +
-            "subject.unique_identifier, subject.id as subject_id, " +
+            "select item_data.id as item_data_id, item_form_metadata.item_data_type_id as item_data_type_id, " +
+            "item_data.tenant_id as tenant_id, study.name as study_name, study.id as study_id, study_site.id as study_site_id, " +
+            "site.name as study_site_name, subject.unique_identifier, subject.id as subject_id, " +
             "study_event_definition.name as event_name, study_event_definition.id as event_def_id, study_event_definition.repeating as event_def_repeating, " +
-            "study_event.occurence as event_occurrence, crf_entry.id as event_crf_id, crf.name as crf_name, crf_version.version_name as crf_version_name, " +
-            "item.variable_name as field_name, item_data.value_index as row, item_data_value.value as field_value " +
+            "(case when study_event.repeating_form_parent_id isnull then study_event.occurence else parent_study_event.occurence end)as event_occurrence, " +
+            "(case when event_definition_crf.repeating then (case when study_event.repeating_form_parent_id isnull then 1 else study_event.occurence end) else null end) as crf_occurrence, " +
+            "crf_entry.id as event_crf_id, crf.name as crf_name, " +
+            "(case when crf_version.version_name isnull or crf_version.version_name = '' then 'original' else crf_version.version_name end) as crf_version_name, " +
+            "item_group_type.lookup_code as item_group_type, item_data.value_index as repeating_group_number,  " +
+            "item.variable_name as field_name, item_data_value.value as field_value " +
             "from rc_item_data item_data " +
             "join rc_item_form_metadata item_form_metadata on item_form_metadata.id = item_data.rc_item_form_metadata_id " +
             "join rc_subjects subject on subject.id = item_data.subject_id " +
@@ -36,13 +40,16 @@ public class RCCConsumer {
             "left join rc_study_sites study_site on study_site.id = subject.study_site_id " +
             "left join rc_sites site on site.id = study_site.site_id " +
             "left join rc_event_crfs crf_entry on crf_entry.id = item_data.event_crf_id " +
+            "left join rc_event_definition_crfs event_definition_crf on event_definition_crf.id = crf_entry.event_definition_crf_id " +
             "left join rc_study_events study_event on study_event.id = crf_entry.study_event_id " +
+            "left join rc_study_events parent_study_event on parent_study_event.id = study_event.repeating_form_parent_id " +
             "left join rc_study_event_definitions study_event_definition on study_event_definition.id = study_event.study_event_definition_id " +
             "left join rc_item_data_values item_data_value on item_data_value.rc_item_data_id = item_data.id " +
-            "where item_data.id IN (?)) t";
+            "left join rc_item_groups item_group on item_group.id = item_form_metadata.group_id " +
+            "left join ad_lookup_codes item_group_type on item_group_type.id = item_group.id " +
+            "where item.variable_name ~* '^[A_Z][A-Z0-9_]+$' and item_data.id IN (?)) t";
 
-
-    private static int c = 0;
+    private static int globalCount = 0;
 
     private static Producer<String, String> producer;
 
@@ -90,7 +97,7 @@ public class RCCConsumer {
 
             System.out.println("start: " + sdf.format(new Date()));
 
-            //
+            //8,943,790
             //start: 2019-08-03 16:09:24
             //end: 2019-08-03 16:24:18
             //15 min
@@ -110,7 +117,7 @@ public class RCCConsumer {
                 if (emptyC > 50 && started && !ids.isEmpty() && !finished) {
                     finished = true;
                     processIds(pgConnectionTo, ids);
-                    System.out.println("total c: " + c);
+                    System.out.println("total count: " + globalCount);
                     System.out.println("end: " + sdf.format(new Date()));
                 }
                 for (ConsumerRecord<String, String> record : records) {
@@ -145,23 +152,30 @@ public class RCCConsumer {
     }
 
     private static void processIds(Connection connectionTo, List<Long> ids) throws SQLException {
-        c += ids.size();
-        System.out.println(" c: " + c);
+        globalCount += ids.size();
+        System.out.println("count: " + globalCount);
         producer.beginTransaction();
         try (ResultSet rs = connectionTo.createStatement().executeQuery(RCC_INFO_SQL.replace("?", ids.toString().replaceAll("\\]|\\[", "")))) {
             if (rs.next()) {
                 Any jsonData = JsonIterator.deserialize(rs.getString(1));
                 for (Any jsonElement : jsonData) {
+                    Any value = jsonElement.get("field_value");
+                    String fieldName = jsonElement.get("field_name").toString();
+                    String fieldValue = value.as(Any.class) != null ? value.toString() : "";
+                    Map<String, Any> map = jsonElement.asMap();
+                    map.remove("field_name");
+                    map.remove("field_value");
+                    map.put(fieldName, Any.wrap(fieldValue));
                     producer.send(new ProducerRecord<>(
                             ES_TOPIC,
-                            jsonElement.get("item_data_id").toString(),
+                            jsonElement.get("subject_id").toString().concat("_").
+                            concat(jsonElement.get("item_data_id").toString()),
                             jsonElement.toString())
                     );
                 }
             }
         }
         ids.clear();
-        producer.flush();
         producer.commitTransaction();
     }
 }
