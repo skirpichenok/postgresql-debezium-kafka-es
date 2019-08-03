0918150a296c1abedcea0af1694f6e8e3e22614c
Sergey Kirpichenok <skirpichenok@gmail.com>
2019-08-03 16:06:50 +0300

    2

--- src/main/java/com/example/RCCConsumer.java
+++ src/main/java/com/example/RCCConsumer.java
@@ -1,9 +1,7 @@
 package com.example;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.jsoniter.JsonIterator;
+import com.jsoniter.any.Any;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -20,10 +18,6 @@ import java.util.*;
 import java.util.Date;
 
 public class RCCConsumer {
-
-    // CREATE TABLE clinical_data (registertime BIGINT, id BIGINT, unique_identifier VARCHAR, event VARCHAR, repeating VARCHAR, occurence VARCHAR, name VARCHAR, version_name VARCHAR, variable_name VARCHAR, value_index VARCHAR, value VARCHAR) WITH (KAFKA_TOPIC = 'clinical_data', VALUE_FORMAT='JSON', KEY = 'id');
-
-    static int c = 0;
 
     private static final String RCC_INFO_SQL = "select json_agg(t) from (" +
             "select item_data.id as item_data_id, item_data.tenant_id as tenant_id, study.name as study_name, study.id as study_id, " +
@@ -47,11 +41,12 @@ public class RCCConsumer {
             "left join rc_item_data_values item_data_value on item_data_value.rc_item_data_id = item_data.id " +
             "where item_data.id IN (?)) t";
 
+
+    private static int c = 0;
+
     private static Producer<String, String> producer;
 
     private static KafkaConsumer<String, String> consumer;
-
-    private static final JsonParser JSON_PARSER = new JsonParser();
 
     private static final String ES_TOPIC = "test-elasticsearch-sink";
 
@@ -59,19 +54,13 @@ public class RCCConsumer {
 
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) throws Exception {
-        //
         try (Connection pgConnectionTo = DriverManager.getConnection("jdbc:postgresql://localhost:5432/nphase", "postgres", "postgres")) {
-            //ResultSet rs;
             int emptyC = 0;
             boolean started = false;
             boolean finished = false;
             pgConnectionTo.setAutoCommit(false);
             List<Long> ids = new ArrayList<>();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-            //Kafka consumer configuration settings
-            final String groupId = "group01";
-            final String clientId = "client01";
 
             Properties producerProps = new Properties();
             producerProps.put("transactional.id","T1");
@@ -80,29 +69,27 @@ public class RCCConsumer {
             producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
             producerProps.put("schema.registry.url", "http://localhost:8081");
             producerProps.put("batch.size", "100000");
-            producer = new KafkaProducer<String, String>(producerProps);
+
+            producer = new KafkaProducer<>(producerProps);
             producer.initTransactions();
 
             Properties consumerProps = new Properties();
             consumerProps.put("bootstrap.servers", "localhost:9092");
-            consumerProps.put("group.id", groupId);
-            consumerProps.put("client.id", clientId);
+            consumerProps.put("group.id", "group01");
+            consumerProps.put("client.id","client01");
             consumerProps.put("enable.auto.commit", "true");
             consumerProps.put("auto.commit.interval.ms", "1000");
             consumerProps.put("session.timeout.ms", "30000");
             consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
             consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-            consumer = new KafkaConsumer<String, String>(consumerProps);
             List<TopicPartition> topic = Collections.singletonList(new TopicPartition(DB_TOPIC, 0));
+            consumer = new KafkaConsumer<>(consumerProps);
             consumer.assign(topic);
             consumer.seekToBeginning(topic);
 
-            //print the topic name
-            System.out.println("Subscribed to topic=" + DB_TOPIC + ", group=" + groupId + ", clientId=" + clientId);
             System.out.println("start: " + sdf.format(new Date()));
 
-            int idx = 0;
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(100L);
                 if (records == null) {
@@ -118,38 +105,34 @@ public class RCCConsumer {
                     processIds(pgConnectionTo, ids);
                     System.out.println("total c: " + c);
                     System.out.println("end: " + sdf.format(new Date()));
-                    int z = 0;
                 }
                 for (ConsumerRecord<String, String> record : records) {
                     if (record == null) {
                         continue;
                     }
                     String type = "";
-                    JsonObject jsonObject = JSON_PARSER.parse(record.value()).getAsJsonObject();
+                    Any jsonObject = JsonIterator.deserialize(record.value());
                     if (jsonObject == null || jsonObject.get("before") == null || jsonObject.get("after") == null) {
                         continue;
                     }
-                    if (jsonObject.get("before").isJsonNull() && jsonObject.get("after").isJsonNull()) {
+                    if (jsonObject.get("before").as(Any.class) == null && jsonObject.get("after").as(Any.class) == null ) {
                         continue;
                     }
-                    if (jsonObject.get("before").isJsonNull() && !jsonObject.get("after").isJsonNull()) {
+                    if (jsonObject.get("before").as(Any.class) == null  && jsonObject.get("after").as(Any.class) != null ) {
                         type = "INSERT";
                     }
-                    if (!jsonObject.get("before").isJsonNull() && jsonObject.get("after").isJsonNull()) {
+                    if (jsonObject.get("before").as(Any.class) != null  && jsonObject.get("after").as(Any.class) == null ) {
                         type = "DELETE";
                         continue;
                     }
-                    if (!jsonObject.get("before").isJsonNull() && !jsonObject.get("after").isJsonNull()) {
+                    if (jsonObject.get("before").as(Any.class) != null  && jsonObject.get("after").as(Any.class) != null ) {
                         type = "UPDATE";
                         continue;
                     }
-                    ids.add(Long.parseLong(jsonObject.getAsJsonObject("after").get("id").toString()));
-                    //processIds(connectionTo, ps, jsonParser, ids);
+                    ids.add(Long.parseLong(jsonObject.get("after").get("id").toString()));
                     if (ids.size() % 100000 == 0) {
                         processIds(pgConnectionTo, ids);
                     }
-                    // print the offset,key and value for the consumer records.
-                    // System.out.printf("offset = %d, key = %s, value = %s,  time = %s \n", record.offset(), record.key(), record.value(), sdf.format(new Date()));
                 }
             }
         }
@@ -161,15 +144,13 @@ public class RCCConsumer {
         producer.beginTransaction();
         try (ResultSet rs = connectionTo.createStatement().executeQuery(RCC_INFO_SQL.replace("?", ids.toString().replaceAll("\\]|\\[", "")))) {
             if (rs.next()) {
-                JsonElement jsonData = JSON_PARSER.parse(rs.getString(1));
-                if (jsonData instanceof JsonArray) {
-                    for (JsonElement jsonElement : ((JsonArray) jsonData)) {
-                        producer.send(new ProducerRecord<String, String>(
-                                ES_TOPIC,
-                                jsonElement.getAsJsonObject().get("item_data_id").toString(),
-                                jsonElement.getAsJsonObject().toString())
-                        );
-                    }
+                Any jsonData = JsonIterator.deserialize(rs.getString(1));
+                for (Any jsonElement : jsonData) {
+                    producer.send(new ProducerRecord<>(
+                            ES_TOPIC,
+                            jsonElement.get("item_data_id").toString(),
+                            jsonElement.toString())
+                    );
                 }
             }
         }
